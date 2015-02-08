c05b40106852bbca495248364f31d0f7590bf94c
Uzair Farooq <uzair@ztagged.com>
2015-02-08 12:25:58 +0500

    Complete extension: applying filters and downloading images

--- data/contentScripts/app.js
+++ data/contentScripts/app.js
@@ -1,2 +1,164 @@
-$("html").hide();
-alert("hello");
+var brains = new(function(){
+	var me = this;
+	var rulesData = [],
+		fileName ="",
+		folderPath="",
+		siteDomainAsNewFolder=false,
+		downloadCount= 0,
+		globalImageArr = [],
+		processQue = -1;
+
+	function hideCounter(){
+		var x = setTimeout(function(){
+			$(".g-counter").animate({"opacity":"0"}, function () {
+				$(this).hide();
+			});
+			clearTimeout(x);
+		},5000);
+	}
+
+	function displayDownloadCount(count){
+		var text = "Images Detected: "+count;
+
+		$(".g-counter h3").html(text);
+
+		$(".g-counter").show().animate({"opacity":"1"});
+	}
+
+	function parseImgURL(url) {
+		var parser = document.createElement('a'),
+			searchObject = {},
+			queries, split, i;
+		// Let the browser do the work
+		parser.href = url;
+		// Convert query string to object
+		queries = parser.search.replace(/^\?/, '').split('&');
+		for( i = 0; i < queries.length; i++ ) {
+			split = queries[i].split('=');
+			searchObject[split[0]] = split[1];
+		}
+		var data = {
+			protocol: parser.protocol,
+			host: parser.host,
+			hostname: parser.hostname,
+			port: parser.port,
+			pathname: parser.pathname,
+			search: parser.search,
+			searchObject: searchObject,
+			hash: parser.hash
+		};
+		return data;
+	}
+
+	function pattenCheck(imgData){
+		var urlObj = parseImgURL(imgData.src),
+			arrLength = rulesData.length;
+
+		for(var i=0;i<arrLength;){
+			/* Store on Image Sizes */
+			var currentRow = rulesData[i],
+				ruleName   = currentRow[0];
+			if(currentRow[1].length > 0){
+				var patt = new RegExp(currentRow[1]);
+				if(patt.test(imgData.src)){
+
+					var obj = {};
+					obj.imgData = imgData;
+					obj.ruleName = ruleName;
+					obj.urlObj = urlObj;
+					obj.downloadCount = downloadCount;
+
+					globalImageArr.push(obj);
+
+					console.log("regex");
+					downloadCount++;
+					displayDownloadCount(downloadCount);
+					break;
+				}
+			}
+			/* Store on Regex */
+			if(currentRow[2].length > 0){
+				var widthHeight = currentRow[2].split(" x ");
+				if(widthHeight[0] == imgData.width && widthHeight[1] == imgData.height){
+
+					var obj = {};
+					obj.imgData = imgData;
+					obj.ruleName = ruleName;
+					obj.urlObj = urlObj;
+					obj.downloadCount = downloadCount;
+
+					globalImageArr.push(obj);
+					console.log("height");
+					downloadCount++;
+					displayDownloadCount(downloadCount);
+					break;
+				}
+			}
+			i++;
+		}
+	}
+
+	function getImageData(self){
+		var imgData = {};
+			imgData.width  = self.naturalWidth;
+			imgData.height = self.naturalHeight;
+			imgData.src    = self.src;
+		pattenCheck(imgData);
+	}
+
+	function asyncSetup(){
+		processQue++;
+		console.log(processQue);
+		console.log(globalImageArr);
+		if(globalImageArr.length>0){
+			var obj = globalImageArr[processQue],
+				imgData = "",
+				ruleName = "",
+				urlObj = "";
+
+			if(obj.imgData != undefined){
+				imgData = obj.imgData;
+			}
+
+			if(obj.ruleName != undefined){
+				ruleName = obj.ruleName;
+			}
+
+			if(obj.urlObj != undefined){
+				urlObj = obj.urlObj;
+			}
+
+			if(obj.downloadCount != undefined){
+				downloadCount = obj.downloadCount;
+			}
+
+			if(processQue <= globalImageArr.length){
+				self.port.emit("save-img",imgData,ruleName,urlObj,downloadCount);
+			}
+		}
+	}
+
+	function intilizeLocalRulesDataAndDownload(data){
+
+		downloadCount=0;
+		rulesData = data.table;
+		fileName = data.fileName;
+		folderPath = data.folderPath;
+		siteDomainAsNewFolder = data.siteDomainAsNewFolder;
+
+		$("body").prepend('<div class="g-counter"><h3></h3></div>');
+
+		$.each($("img"),function(index,value){
+			getImageData($(this)[0]);
+		});
+
+		asyncSetup();
+		hideCounter();
+	}
+
+	this.init = function(){
+		self.port.on("send-inti-data-to-contentScript",intilizeLocalRulesDataAndDownload);
+		self.port.on("send-next-img",asyncSetup);
+	}
+});
+brains.init();
--- data/contentScripts/options.js
+++ data/contentScripts/options.js
@@ -9,16 +9,8 @@ var addRule = new(function(){
 		imageWidth  = $("#image-width"),
 		addBtn      = $(".add-btn"),
 		deleteBtn   = $(".delete-btn"),
-		saveBtn     = $(".save-btn");
-
-	this.init = function(){
-		addBtn.on("click",addRow);
-		tr.on("click",highlightRow);
-		deleteBtn.on("click",deleteRow);
-		saveBtn.on('click',save);
-		load_data();
-
-	};
+		saveBtn     = $(".save-btn"),
+		poweringOff = $(".power-off");
 
 	function addRow(){
 		var nextRow = table[0].rows.length,
@@ -26,7 +18,7 @@ var addRule = new(function(){
 			cell1           = row.insertCell(0),
 			cell2           = row.insertCell(1),
 			cell3           = row.insertCell(2),
-			cell4           = row.insertCell(2);
+			cell4           = row.insertCell(3);
 
 		cell1.innerHTML = nextRow;
 		cell2.innerHTML = ruleName.val();
@@ -48,6 +40,14 @@ var addRule = new(function(){
 		}
 	}
 
+	function deleteAllRows(){
+		$.each($(".table tr"),function(index,value){
+			if(!($(this).hasClass("uneditable"))){
+				$(this).remove();
+			}
+		});
+	}
+
 	function deleteRow(){
 		$(".warning").remove();
 		var rowLength = $(".table")[0].rows.length-1;
@@ -64,7 +64,7 @@ var addRule = new(function(){
 			table:[],
 			fileName              :$("#file-name").val(),
 			folderPath            :$("#folder-path").val(),
-			siteDomainAsNewFolder :false
+			appStatus             :$(".app-status").attr("status")
 		};
 		var rowLength = $(".table")[0].rows.length-1;
 		for(var i = 1; i<= rowLength;){
@@ -80,16 +80,110 @@ var addRule = new(function(){
 		return data;
 	}
 
+	function powerOff(){
+		var cacheSav =  $(".app-status"),
+			flag = cacheSav.attr("status");
+
+		var text1="",
+			text2="";
+		console.log("Status"+flag);
+		if(flag == "true"){
+			text1 = "Powering Off";
+			text2 = "Good Bye!";
+			flag = "false";
+			cacheSav.attr("status",flag);
+		}else{
+			text1 = "Powering On";
+			text2 = "We are live!";
+			flag = "true";
+			cacheSav.attr("status",flag);
+		}
+
+		cacheSav.html(text1);
+
+		cacheSav.show().animate({"opacity":"1"});
+
+		var y = setTimeout(function(){
+			cacheSav.html(text2);
+			clearTimeout(y);
+		},1000);
+
+		var x = setTimeout(function(){
+			cacheSav.animate({"opacity":"0"}, function () {
+				$(this).hide();
+			});
+			clearTimeout(x);
+			self.port.emit("on-off-app",flag);
+		},1500);
+	}
+
 	function save(){
+		var text = "Saving...",
+			cacheSav = $(".saving-options");
+
+		cacheSav.html(text);
+
+		cacheSav.show().animate({"opacity":"1"});
+
+		var y = setTimeout(function(){
+			var text = "Saved!";
+			cacheSav.html(text);
+			clearTimeout(y);
+		},1000);
+
+		var x = setTimeout(function(){
+			cacheSav.animate({"opacity":"0"}, function () {
+				$(this).hide();
+			});
+			clearTimeout(x);
+		},1500);
 		console.log(packageDataToSend());
 		service.send(service.sendDataFromSettingToApp,packageDataToSend());
 	}
 
+	function clearAllFields(){
+		deleteAllRows();
+		patternRx.val("");
+		ruleName.val("");
+		imageHeight.val("");
+		imageWidth.val("");
+		$("#file-name").val("");
+		$("#folder-path").val("");
+	}
+
 	function load_data(){
-		self.port.on("load-init-data", function() {
-			alert("hello");
-			$("html").hide();
+		self.port.on("load-init-data", function(data) {
+			clearAllFields();
+			var rowLength = data.table.length;
+			for(var i=0;i<rowLength;){
+				var rowCount = i+ 1,
+					row   = $(".table")[0].insertRow(rowCount),
+					cell0 = row.insertCell(0);
+					cell0.innerHTML = (rowCount);
+				for(var y=0;y<=2;){
+					var cellCount = y+1;
+					var cell = row.insertCell(cellCount);
+					cell.innerHTML = data.table[i][y];
+					y++;
+				}
+				i++;
+			}
+			$("#file-name").val(data.fileName);
+			$("#folder-path").val(data.folderPath);
+			$(".table tr").on("click",highlightRow);
+			$(".app-status").attr("status",data.appStatus);
 		});
 	}
+
+	this.init = function(){
+		addBtn.on("click",addRow);
+		tr.on("click",highlightRow);
+		deleteBtn.on("click",deleteRow);
+		saveBtn.on('click',save);
+		poweringOff.on('click',powerOff);
+		load_data();
+
+		console.log($(".app-status").attr("status"));
+	};
 });
 addRule.init();
